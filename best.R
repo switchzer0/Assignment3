f1a6a764c00256c858f5b8db21faafb56242b777
switchzer0 <adam.spar@gmail.com>
2017-01-25 10:19:18 -0500

    work from holidays

--- best.R
+++ best.R
@@ -1,28 +1,27 @@
 best <- function(state, outcome) {
-        ## Read outcome data subsetting selecting relevant columns
-        outcome_options <- c("Heart Attack"=11, "Heart Failure"=17, "Pneumonia"=23)
-        outcome_data <- read.csv("outcome-of-care-measures.csv")[,c(2,7,outcome_options[outcome])]
-        
-        
-        ## Check that state and outcome are valid
+        #set outcome options and check that state and outcome are valid
+        outcome_options <- c("heart attack"=11, "heart failure"=17, "pneumonia"=23)
         if(!state %in% state.abb) stop("invalid state")
         if(!outcome %in% names(outcome_options)) stop("invalid outcome")
-        
+
+
+        ## Read outcome data subsetting selecting relevant columns
+        outcome_data <- read.csv("outcome-of-care-measures.csv")[,c(2,7,outcome_options[outcome])]
+
 
         ## subset data to state indicated in function and then select min hospital for that outcome
-        state_select <- outcome_data[outcome_data$State == state,]
-        state_select[,outcome],na.rm = TRUE
         
-        ##find the row, save to variable, print the name col of that row?
-                ##outcome options are 
-                #Column 11 is heart attack mortality
-                #Column 17 is heart failure mortality
-                #Column 23 is pneumonia mortality
+        data_by_state <- outcome_data[outcome_data$State == state,]
+        sortorder <- c(names(data_by_state[3]),names(data_by_state[1]))
         
-                #drop NA values before ranking options
-                                        
-        ## Return hospital name in that state with lowest 30-day death rate
-                ## Column 2 is hospital name
-                                        
-                                        
+        sorted <- data_by_state[order(names(data_by_state[3]),names(data_by_state[1])),]
+                
+                
+                ##template: with(score, score[order(sex, y, x),])
+                sortorder <- c(names(data_by_state[2]),names(data_by_state[3]),names(data_by_state[1]))
+                with(data_by_state, data_by_state[order(sortorder),])
+                
+                
+                best_row <- which.min(data_by_state[,3])
+        data_by_state$Hospital.Name[best_row]
 }
