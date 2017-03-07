a244cf87ba9ec618c485be48a6fc3f95436da12e
switchzer0 <adam.spar@gmail.com>
2017-03-07 00:16:27 -0500

    restructured commands to be simple but lengthier

--- best.R
+++ best.R
@@ -1,27 +1,20 @@
 best <- function(state, outcome) {
-        #set outcome options and check that state and outcome are valid
-        outcome_options <- c("heart attack"=11, "heart failure"=17, "pneumonia"=23)
+        ## Read outcome data, identify columns
+        outcome_data <- read.csv("outcome-of-care-measures.csv")
+        outcome_options <- c("heart attack", "heart failure", "pneumonia")
+        colnames(outcome_data)[c(11,17,23)] <- outcome_options
+        
+        
+        #check that state and outcome are valid
         if(!state %in% state.abb) stop("invalid state")
-        if(!outcome %in% names(outcome_options)) stop("invalid outcome")
-
-
-        ## Read outcome data subsetting selecting relevant columns
-        outcome_data <- read.csv("outcome-of-care-measures.csv")[,c(2,7,outcome_options[outcome])]
+        if(!outcome %in% outcome_options) stop("invalid outcome")
 
 
         ## subset data to state indicated in function and then select min hospital for that outcome
-        
-        data_by_state <- outcome_data[outcome_data$State == state,]
-        sortorder <- c(names(data_by_state[3]),names(data_by_state[1]))
-        
-        sorted <- data_by_state[order(names(data_by_state[3]),names(data_by_state[1])),]
-                
-                
-                ##template: with(score, score[order(sex, y, x),])
-                sortorder <- c(names(data_by_state[2]),names(data_by_state[3]),names(data_by_state[1]))
-                with(data_by_state, data_by_state[order(sortorder),])
-                
-                
-                best_row <- which.min(data_by_state[,3])
-        data_by_state$Hospital.Name[best_row]
+        state_subset <- outcome_data[outcome_data$State == state,]
+        outcome_subset <- state_subset[state_subset$outcome == !is.na]
+        sorted_outcome <-  state_subset[order(state_subset$Hospital.Name),]
+        ##sorted_outcome[sorted_outcome$]
+        best_row <- which.min(sorted_outcome[[outcome]])
+        as.character(sorted_outcome$Hospital.Name[best_row])
 }
