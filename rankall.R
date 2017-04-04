d4b26b422f681b8a9a55dbe65ef1793d9c9c394f
switchzer0 <adam.spar@gmail.com>
2017-04-04 08:43:18 -0400

    finished rankall function except for rbind issue to create final data frame

--- rankall.R
+++ rankall.R
@@ -10,26 +10,27 @@ rankall <- function(outcome, num = "best") {
         
         #order data and remove hospitals w/ NA on the given outcome
         outcome_subset <-  outcome_data[order(as.numeric(outcome_data[[outcome]]),outcome_data$Hospital.Name, na.last = NA),]
- 
-        #define index parameters
-        if (num == "best") {index <- 1
-                } else if (num == "worst") {index <- nrow(sorted_outcome)
-                } else index <- num
         
         #loop through state list, pulling the "outcome" at "num" rank from each one and adding it to a data frame
         ##alternatively, order() by state and then by rank (ascending) and loop selecting 4, 4+50, etc.
-        dat <- data.frame()
+        dat <- data.frame(stringsAsFactors = FALSE)
         #loop through files, running cor() on those those greater than threshold
         #does the assignment need state names in alpha order, or the abbrevs? will need to rank state.abb if the former
         for (i in 1:length(state.abb)) {
                 #get just the next state subset in the list
                 ranked <- outcome_subset[outcome_subset$State == state.abb[i],]
+                #define index parameters
+                if (num == "best") {index <- 1
+                        } else if (num == "worst") {index <- nrow(ranked)
+                        } else index <- num
                 #grab the hospital name per specified rank ("num)
-                hosp_name <- as.character(ranked$Hospital.Name[index])
+                hosp_name <- ranked$Hospital.Name[index]
                 
                 #append the hospital name and state name to data frame
-                dat <- rbind(dat, c(hosp_name, state.abb[i]))
+                ###ISSUE WITH rbind and "invalid factor level NA generated"
+                dat <- rbind.data.frame(dat, c(hosp_name, state.abb[i]))
                 }
+        colnames(dat) <- c("hospital", "state")
         dat
 }
 
